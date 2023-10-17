ab009a26072015ff8fe24bfe576eb83e5f579576
Andrei Pereira <48697839+andreiPereira@users.noreply.github.com>
2023-10-17 10:09:23 -0300

    Instalado MUI e desacoplado input

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
-import GoodForm from "./components/GoodForm";
-import BadForm from "./components/BadForm";
 
-import "./App.css";
+import { useForm, Controller } from "react-hook-form";
+import { Button, Grid, TextField } from "@mui/material";
+import FormInput from "./components/FormInput";
 
 const App = () => {
-  return <GoodForm />;
+
+  const { handleSubmit, control, setValue } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+
+    <form onSubmit={handleSubmit(onSubmit)}>
+
+      <Grid container sx={{ mt: 4 }} px={6}>
+
+      <FormInput label="Login" name="user" control={control} />
+      <FormInput label="Senha" name="password" control={control} />
+      <FormInput label="Idade" name="age" control={control} />
+
+        <Grid item xs={12} sm={12} sx={{ padding: '10px' }}>
+          <Button variant="contained" fullWidth type="submit">Submit</Button>
+        </Grid>
+
+      </Grid>
+
+
+
+    </form>
+  );
 };
 
 export default App;
---
+++ src/components/FormInput.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Controller } from 'react-hook-form';
+import { Grid, TextField } from '@mui/material';
+
+const FormInput = ({ name, control, label }) => {
+  return (
+    <Grid item xs={12} sm={12} sx={{ padding: '10px' }}>
+      <Controller
+        name={name}
+        control={control}
+        defaultValue=""
+        render={({ field }) => (
+          <TextField
+            {...field}
+            label={label.charAt(0).toUpperCase() + label.slice(1)} // Usa o nome para gerar um label capitalizado
+            variant="outlined"
+            fullWidth
+          />
+        )}
+      />
+    </Grid>
+  );
+};
+
+export default FormInput;
